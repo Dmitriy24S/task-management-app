1716bf58d323dd51d9ff44f4e30aa6eb6c8d7acf
Dmitriy24S <devilins24@gmail.com>
2022-09-05 08:56:00 +0300

    style: make darker modal backdrop/overlay, header mobile spacing (gap,text size), logo width/height adjustment

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -26,28 +26,28 @@ const Header = ({
       <div className="flex items-center justify-between p-4">
         <div className="header-left flex items-center gap-3">
           {/* Logo */}
-          <div className="hidden sm:mr-4 sm:block">
+          <div className="hidden sm:mr-4 sm:mb-[1px] sm:flex sm:items-end">
             <Image
               src={`/assets/icons/logo-${darkTheme ? "light" : "dark"}.svg`}
               height={26}
-              width={153}
+              width={161}
               layout="fixed"
               alt="logo"
               priority
             />
           </div>
-          <div className="sm:hidden">
-            <Image src="/assets/icons/logo-mobile.svg" height={24} width={25} alt="logo" />
+          <div className="flex items-end sm:hidden">
+            <Image src="/assets/icons/logo-mobile.svg" height={25} width={25} alt="logo" />
           </div>
           {/* Active board name / open/close sidebar menu */}
           <button
-            className="relative flex items-center gap-1 text-2xl font-bold before:absolute before:-inset-1 before:top-full before:left-0 before:block before:h-0.5 before:w-0 before:bg-[#635FC7] before:opacity-100 before:transition-all before:duration-300 hover:before:w-full hover:before:opacity-100 sm:focus:outline-offset-2 sm:focus-visible:outline-offset-2"
+            className="relative flex items-center gap-1 text-base font-bold before:absolute before:-inset-1 before:top-full before:left-0 before:block before:h-0.5 before:w-0 before:bg-[#635FC7] before:opacity-100 before:transition-all before:duration-300 hover:before:w-full hover:before:opacity-100 sm:text-2xl sm:focus:outline-offset-2 sm:focus-visible:outline-offset-2"
             onClick={() => setIsBoardMenuOpen(!isBoardMenuOpen)}
           >
             {selectedBoard.name} <FaChevronDown fill="#635FC7" className="sm:hidden" />
           </button>
         </div>
-        <div className="header-right flex items-center gap-1.5">
+        <div className="header-right flex items-center sm:gap-1.5">
           <HeaderAddTaskButton setIsNewTaskFormOpen={setIsNewTaskFormOpen} />
           <HeaderMoreInfoButton />
         </div>
