86c6e66de01bd0cfb4f537930d33df0e5c1e3a18
Dmitriy24S <devilins24@gmail.com>
2022-09-14 08:27:29 +0300

    fix: add higher z-index to modal -> fix mobile modals backdrop overlap order when open sidemenu + new board modal

--- src/components/Shared/Modal.tsx
+++ src/components/Shared/Modal.tsx
@@ -12,11 +12,12 @@ const Modal = ({ children, isOpen, setIsOpen }: Props) => {
   return (
     <div className={`modal-wrapper ${isOpen ? "block max-h-screen" : "hidden"} `}>
       <div
-        className={`backdrop fixed inset-0 z-20 bg-black opacity-60`}
+        // set backdrop z-index 40 and content z-index 50 (to be on top of mobile sidebar menu window - otherwise overlap 2x backdrops -> messes with click on backdrop closes sidemenu first while new board modal is open)
+        className={`modal-backdrop fixed inset-0 z-40 bg-black opacity-60`}
         // onClick={() => setIsOpen(false)}
         onClick={() => setIsOpen(null)}
       ></div>
-      <section className="content-container fixed left-0 right-0 z-30 mx-auto mt-20 flex max-h-[80vh] w-11/12 min-w-[20rem] max-w-md flex-col gap-6 overflow-y-auto rounded-lg bg-white p-6 text-black dark:bg-[#2B2C37] dark:text-white sm:max-w-md">
+      <section className="modal-content-container fixed left-0 right-0 z-50 mx-auto mt-20 flex max-h-[80vh] w-11/12 min-w-[20rem] max-w-md flex-col gap-6 overflow-y-auto rounded-lg bg-white p-6 text-black dark:bg-[#2B2C37] dark:text-white sm:max-w-md">
         {children}
       </section>
     </div>
